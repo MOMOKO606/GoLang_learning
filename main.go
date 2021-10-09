b16b7ebbb27caa68df0530b75de4efd13681f03c
BianLong0618 <bianlong0618@qq.com>
2021-10-09 16:21:35 +0800

    Add some features about slice and its cap in Golang.

--- main.go
+++ main.go
@@ -77,6 +77,22 @@ func main() {
 	//  Method 2. declare & assign simultaneously.
 	//  定义了一个长度为8，并给前5个数赋初值，其他值默认为0。
 	arr02 := [8]int{0, 1, 2, 3, 4}
+	arr03 := [...]float64{11, 12, 13, 16, 23, 26, 28, 30} //  省略元素长度，Go会自动根据元素个数来计算长度。
+
+	//  二维数组
+	arr04 := [...][2]float64{[2]float64{1, 3}, [2]float64{2, 4}}
+	//  上式可以简化为：
+	//  arr04 := [...][2]float64{{1,3}, {2,4}}
+
+	//  2.2.5  slice "动态数组"
+	var s01, s02 []float64
+	s01 = arr03[0:3]
+	s02 = arr03[4:6]
+	//  Test for slice, slice's cap & array.
+	//  对array切片后得到slice，slice对应的cap是它的起点到其original array结尾的长度。
+	var arr05 [10]int
+	s03 := arr05[2:4]   // s03.cap为从arr05的index 2到9的长度，8.
+	s04 := arr05[2:4:7] //  指定cap到index=7但不包含7 => cap = 5
 
 	fmt.Println("a0 =", a0)
 	fmt.Println("a1 + a2 + a3 =", a1+a2+a3)
@@ -88,4 +104,9 @@ func main() {
 	fmt.Println("%s\n", message)
 	fmt.Println("Test iota:", a5, a6, a7, a8, a9)
 	fmt.Println("Show the array arr02: ", arr02)
+	fmt.Println("Show the array arr03: ", arr03)
+	fmt.Println("Show the 2D array arr04: ", arr04)
+	fmt.Println("Show the slice s01 & s02: ", s01, s02)
+	fmt.Println("Show the cap of s03: ", cap(s03))
+	fmt.Println("Show the cap of s04: ", cap(s04))
 }
