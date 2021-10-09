337fe0b1b8909d3bae2395326ba6ce16251ca55e
BianLong0618 <bianlong0618@qq.com>
2021-10-09 14:11:31 +0800

    Add some features about iota.

--- main.go
+++ main.go
@@ -15,9 +15,16 @@ func main() {
 	var _ int = 4
 	//  同时定义多个变量.
 	var a1, a2, a3 int = 5, 6, 7
+	//  另一种定义多个变量的方法：分组声明.
+	//var (
+	//	a1 = 5
+	//	a2 = 6
+	//	a3 = 7
+	//)
 	//  可以省略变量类型，compiler会根据等号右边的数据类型自动分配。
 	var b1, b2, b3 = a1, a2, a3
 	//  也可以省略关键字var，直接用:=，像Python和MATLAB那样。
+	//  Notice: var搭配= 和 := 作用相等。
 	_, a4 := 1988, 8
 
 	//  boolean类型.
@@ -35,14 +42,29 @@ func main() {
 	world := "World!"
 	str := hello + " " + world
 
-	// One way for changing the characters in string.
+	//  One way for changing the characters in string.
 	tmp := []byte(str) // transfer string to byte array.
 	tmp[5] = '_'
 	str = string(tmp) //  transfer byte array to string.
 
+	//  多行字符串.
+	//  ``括起的字符串为Raw字符串，原模原样print。
+	message := ` This is Golang, 
+				 next line.`
+
 	//  2.2.2 constant.
 	const Pi float64 = 3.1415926
 	const MaxThread = 10
+
+	//  2.2.3  iota.
+	//  默认值为0，iota在同一行值相同，不同行默认加1.
+	//  每遇到一个const关键字，iota就会重置为0.
+	const (
+		a5, a6 = iota, iota
+		a7     = iota
+		a8     = iota
+	)
+	const a9 = iota
 
 	fmt.Println("a0 =", a0)
 	fmt.Println("a1 + a2 + a3 =", a1+a2+a3)
@@ -51,4 +73,6 @@ func main() {
 	fmt.Println("Is Go good?", isGogood, "\n", "Is Go bad?", isGobad)
 	fmt.Println("An example of multiplication of complex nums: ", c0*c1)
 	fmt.Println("Again:", str)
+	fmt.Println("%s\n", message)
+	fmt.Println("Test iota:", a5, a6, a7, a8, a9)
 }
