31afa7af05bf625fd2a09cb9316c9878ceff56b1
BianLong0618 <bianlong0618@qq.com>
2021-10-09 11:51:58 +0800

    Add 2.2.1 variable part.

--- main.go
+++ main.go
@@ -19,10 +19,18 @@ func main() {
 	var b1, b2, b3 = a1, a2, a3
 	//  也可以省略关键字var，直接用:=，像Python和MATLAB那样。
 	_, a4 := 1988, 8
+	//  boolean类型.
+	var isGogood bool //  The default value of a bool variable is false.
+	isGogood = true
+	isGobad := false
+
+	//  2.2.2 constant.
+	const Pi float64 = 3.1415926
+	const MaxThread = 10
 
 	fmt.Println("a0 =", a0)
 	fmt.Println("a1 + a2 + a3 =", a1+a2+a3)
 	fmt.Println("b1 + b2 + b3 =", b1+b2+b3)
 	fmt.Println("a4 =", a4)
-
+	fmt.Println("Is Go good?", isGogood, "\n", "Is Go bad?", isGobad)
 }
